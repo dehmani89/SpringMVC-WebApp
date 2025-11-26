d96b2bb5acddc11365378bea8c723f03845066a3
Amine Dehmani <amine.dehmani@gmail.com>
2025-11-26 00:06:51 -0500

    Update user credentials in LoginService

--- src/main/java/com/springmvc/login/LoginService.java
+++ src/main/java/com/springmvc/login/LoginService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service	//using the @Service annotation makes the LoginService class available as a bean
 public class LoginService {
 	public boolean validateUser(String user, String password) {
-		return user.equalsIgnoreCase("amine89") && password.equals("dehmani89");
+		return user.equalsIgnoreCase("test1234") && password.equals("test1234");
 	}
 
 }
